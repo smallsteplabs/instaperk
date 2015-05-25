f4fe1e47e43caa0eb1a44aef8691c25fb1bdfc58
Wiriana Awiputra <rianawi@gmail.com>
2015-05-25 15:07:05 +0700

    Add dynamic data

--- js/app.js
+++ js/app.js
@@ -6,12 +6,29 @@ var Header = React.createClass({
   }
 });
 
+var BizListItem = React.createClass({
+  render: function () {
+    var biz = this.props.biz;
+
+    return (
+      <li>
+        <strong>{biz.name}</strong> {biz.address}
+      </li>
+    );
+  }
+});
+
 var BizList = React.createClass({
   render: function () {
+    var businesses = this.props.businesses.map(function(biz) {
+          return (
+            <BizListItem key={biz.id} biz={biz} />
+          )
+        });
+
     return (
       <ul>
-        <li>Dean's Downtown</li>
-        <li>Roma's Pizza</li>
+        {businesses}
       </ul>
     );
   }
@@ -19,10 +36,17 @@ var BizList = React.createClass({
 
 var HomePage = React.createClass({
   render: function () {
+    var businesses= [
+      {id: 1, name: "Dean's Downtown", address: "316 Main St, Houston, TX"},
+      {id: 2, name: "Roma's Pizza", address: "233 Main St, Houston, TX"},
+      {id: 3, name: "Fusion Taco", address: "801 Congress, Houston, TX"},
+      {id: 4, name: "Frank's Pizza", address: "417 Travis St, Houston, TX"}
+    ];
+
     return (
       <div>
         <Header text="Instaperk"/>
-        <BizList />
+        <BizList businesses={businesses} />
       </div>
     );
   }
